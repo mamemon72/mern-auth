78a3f8cec2485fafac9e6a24fcd422f2ffd34eb0
Muhammad Memon <ameenmemon2001@gmail.com>
2025-02-02 16:48:08 +0000

    connect to DB

--- api/index.js
+++ api/index.js
@@ -1,6 +1,17 @@
 import express from 'express';
+import mogoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const app = express();
+
+mogoose.connect(process.env.MONGO)
+.then(()=>{
+    console.log("Connected to MongoDB");
+    })
+    .catch((err)=>{
+       console.log(err);});
+
+      const app = express();
 app.listen(3000, () => {
-  console.log('Server listening on port 3000');        
-}); 
+  console.log('Server listening on port 3000')    ;  
+});
